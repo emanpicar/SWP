7b554e25cd737febaedeec4038e8223f7e76604d
Andrel del Rosario <andreldelrosario@gmail.com>
2019-12-18 06:27:28 +0800

    Tried to do integration, but can't seem to detect bluetooth devices

--- app/src/main/java/com/nokia/swp/MainActivity.kt
+++ app/src/main/java/com/nokia/swp/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.nokia.swp
 
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.service.autofill.OnClickAction
+import android.util.Log
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
@@ -12,12 +19,81 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
+    private var m_bluetoothAdapter: BluetoothAdapter? = null
+    private val bTItems: HashMap<String, Short> = LinkedHashMap()
+    private val REQUEST_ENABLE_BLUETOOTH = 1
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         navigationSetup()
         radarBG()
+
+        m_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        if(m_bluetoothAdapter == null) {
+            Log.i("","This device does not support BT")
+            return
+        }
+        run()
+    }
+
+    private fun run() {
+        if(!m_bluetoothAdapter!!.isEnabled) {
+            val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BLUETOOTH)
+        }
+        discoverDevices()
+    }
+
+    private fun discoverDevices() {
+        if (m_bluetoothAdapter!!.isEnabled) {
+            scanDevices()
+        }
+    }
+
+    private val mFoundReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val action = intent.action
+            if (BluetoothDevice.ACTION_FOUND == action) {
+                val device = intent.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
+                val signal = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, java.lang.Short.MIN_VALUE)
+                if (device?.name != null) {
+                    plot(device.name, signal)
+                }
+            }
+        }
+    }
+
+    private fun scanDevices(){
+        if (m_bluetoothAdapter!!.isDiscovering) {
+            // Bluetooth is already in mode discovery mode, we cancel to restart it again
+            m_bluetoothAdapter!!.cancelDiscovery()
+        }
+        val bool = m_bluetoothAdapter?.startDiscovery()
+        Log.i("", bool.toString())
+        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        registerReceiver(mFoundReceiver, filter)
+    }
+
+    private fun plot(name: String, signal: Short) {
+        Log.i("Device", name + "(" + signal + ")" )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_ENABLE_BLUETOOTH) {
+            if (resultCode == Activity.RESULT_OK) {
+                if (m_bluetoothAdapter!!.isEnabled) {
+                    Log.i("Device", "BT has been enabled")
+                    discoverDevices()
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                Log.i("Device", "BT enabling has been cancelled")
+            }
+        }
+
     }
 
     fun navigationSetup() {
